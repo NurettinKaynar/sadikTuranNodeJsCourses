9f8bcaa2af8574b5b191e1f26ac47040edf18140
Nurettin KAYNAR <nurettin.kaynar@ardgrup.com.tr>
2024-02-23 10:44:32 +0300

    lesson4

---
+++ chapter1/lesson4/app.js
@@ -0,0 +1,52 @@
+const http=require('http')
+
+
+const requestListener=(req,res)=>{
+    if(req.url==="/"){
+        res.writeHead(200,{"Content-Type":"text/html"})
+        res.write(`
+        <html>
+        <head>
+        <title> Home </title>
+        </head>
+        <body>
+        <h1>Home</h1>
+        </body>
+        </html
+        `)
+    }
+    else if( req.url==="/blogs"){
+        res.writeHead(200,{"Content-Type":"text/html"})
+        res.write(`
+        <html>
+        <head>
+        <title> Blogs</title>
+        </head>
+        <body>
+        <h1>Blogs</h1>
+        </body>
+        </html
+        `)
+    }
+    else{
+        res.writeHead(404,{"Content-Type":"text/html"})
+        res.write(`
+        <html>
+        <head>
+        <title> Page Not Found</title>
+        </head>
+        <body>
+        <h1>Page Not Found</h1>
+        </body>
+        </html
+        `)
+    }
+
+    res.end()
+}
+
+const server=http.createServer(requestListener)
+
+server.listen(3000)
+
+console.log(`Node.JS server listening on 3000`);
